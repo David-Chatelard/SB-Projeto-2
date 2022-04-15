ab3865e9edf04035203edc12342c4d2eda3760ed
David Fanchic Chatelard <davidfchatelard@gmail.com>
2022-04-15 12:55:30 -0300

    Le os opcodes do arquivo, mas da seg fault, pois para em TAM

--- simulador.asm
+++ simulador.asm
@@ -4,11 +4,20 @@ size_input_command EQU $-input_command
 
 output_file_name db "saida.asm", 0
 
+TAM dd 150
+
 section .bss
 input_file_name resb 25 ;reserva 25 bytes
 input_file_descriptor resd 1
 
 output_file_descriptor resd 1
+
+str_input resb 150 ;considerando que o arquivo de entrada tera no maximo 150 bytes
+
+opcode_lido resb 1
+
+i resd 1 ;para botar TAM em ecx no for
+index resd 1
 
 section .text
 global _start
@@ -46,13 +55,52 @@ _start:
 	
 	mov dword [output_file_descriptor], eax  ;salvando o file descriptor
 	
-	;Ler os opcodes
+	;Chamada 3, input, para ler do arquivo de entrada
+	mov eax, 3
+	mov ebx, [input_file_descriptor]
+	mov ecx, str_input
+	mov edx, TAM ;conteudo do arquivo so vai ate 150 bytes
+	int 80h
+
+	;Chamada 4, print na tela para testar
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, str_input
+	mov edx, TAM
+	int 80h
+
+	;Loop para ler os opcodes
+	mov dword [i], TAM ;salvando TAM para por no ecx
+	mov dword ecx, [i] ;botando TAM no ecx para o loop
+	mov dword [index], 0
+loop_begin:
+	mov ebx, [index]
+	mov al, byte [str_input+ebx]
+	mov byte [opcode_lido], al
+
+	push ecx
+
+	;Chamada 4, print na tela para testar
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, opcode_lido
+	mov edx, 1
+	int 80h
+
+	pop ecx
+	inc dword [index]
+
+	loop loop_begin
 	
-	;Chamada , para fechar o arquivo
+	;Chamada 6, para fechar o arquivo
 	mov eax, 6
 	mov ebx, dword [input_file_descriptor]
 	int 80h
-	
+
+	;Chamada 6, para fechar o arquivo
+	mov eax, 6
+	mov ebx, dword [output_file_descriptor]
+	int 80h
 	
 	;Finalizando o programa
 	mov eax, 1
